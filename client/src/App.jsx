ea5d492aa8860d13ac0ec087c764e4f998de21e9
mintyry <gitmintyry@gmail.com>
2024-01-18 15:22:48 -0800

    finished accordion and entire tour page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,19 @@
-
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import SearchAppBar from './components/AppBar';
-import TourCard from './components/Tourcard';
-import Container from '@mui/material/Container';
-import Grid from "@mui/material/Grid";
-import cities from "./data.json";
-import { Typography } from '@mui/material';
+import Tour from './pages/Tour';
 
 
 function App() {
-
-
   return (
-    <>
-      <SearchAppBar />
-      <Container sx={{ marginY: 5 }}>
-        {cities.map((city) => (
-          <>
-            <Typography variant="h4" component="h2" marginTop={5} marginBottom={3}>
-              Top {city.name} Tours
-            </Typography>
-            <Grid container spacing={5}>
-             {city.tours.map((tour, index) => <TourCard tour={tour} key={index} />)}
-            </Grid>
-          </>
-        ))}
-
-      </Container>
-    </>
+    <BrowserRouter>
+    <SearchAppBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Tour />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/Tour.jsx
@@ -0,0 +1,37 @@
+// This wont be dynamic, bc video guy didnt wanna spend time on that
+
+import { Accordion, Typography } from "@mui/material";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box"
+import ImageCollage from "../components/ImageCollage";
+import CustomizedAccordions from "../components/Accordion";
+
+const Tour = () =>
+    <Container sx={{ width: 900 }}>
+
+        <Typography variant="h3" component="h1" marginTop={3}>
+            Explore the world in Vegas
+        </Typography>
+        <Box marginTop={3} sx={{display: "flex"}} >
+            <img src="https://www.adorama.com/alc/wp-content/uploads/2018/11/landscape-photography-tips-yosemite-valley-feature.jpg" alt="picture" height={325}  />
+            <ImageCollage />
+        </Box>
+        <Box>
+            <Typography variant="h6" component="h4" marginTop={3}>
+                About this ticket
+            </Typography>
+            <Typography variant="paragraph" component="p" marginTop={3}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, tenetur. Debitis, magnam doloribus vero ipsa eius expedita, corporis quod sequi voluptatibus aliquam iure laboriosam recusandae nobis. Expedita suscipit sunt eligendi?
+            </Typography>
+        </Box>
+        <Box marginBottom={8}>
+            <Typography variant="h6" component="h4" marginTop={3} marginBottom={2}>
+                FAQ
+            </Typography>
+            <CustomizedAccordions />
+        </Box>
+        
+    </Container>;
+
+export default Tour;
+
