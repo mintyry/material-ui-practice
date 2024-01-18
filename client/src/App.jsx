05a2d1c5f5989d20f8f9639276d34d49e86bea90
mintyry <gitmintyry@gmail.com>
2024-01-18 13:45:32 -0800

    added appbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css';
+import SearchAppBar from './components/AppBar';
 import TourCard from './components/Tourcard';
 import Container from '@mui/material/Container';
 import Grid from "@mui/material/Grid";
@@ -10,7 +11,8 @@ function App() {
 
   return (
     <>
-      <Container>
+    <SearchAppBar />
+      <Container sx={{ marginY: 5 }}>
         <Grid container spacing={5}>
           <TourCard />
           <TourCard />
