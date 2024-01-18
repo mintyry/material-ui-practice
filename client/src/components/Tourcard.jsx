c8f86e79d3dc1e3912c5c7917e57899d46ae7488
mintyry <gitmintyry@gmail.com>
2024-01-18 10:52:12 -0800

    included rating and review

--- client/src/components/Tourcard.jsx
+++ client/src/components/Tourcard.jsx
@@ -3,6 +3,8 @@ import Grid from "@mui/material/Grid";
 import Typography from '@mui/material/Typography';
 // acts as a div, wrapper for all other elements we want
 import Box from '@mui/material/Box';
+import {AccessTime} from '@mui/icons-material';
+import Rating from '@mui/material/Rating';
 
 const TourCard = () => {
     return (
@@ -14,13 +16,34 @@ const TourCard = () => {
                     <Typography variant="subtitle1" component="h2">
                         Immerse into the Scenery
                     </Typography>
+                    <Box sx={{
+                        display: "flex",
+                        alignItems: "center"
+                    }}>
+                        <AccessTime sx={{width: 12.5}}/>
+                        <Typography variant="body2" component="p" marginLeft={0.5}>
+                            5 hours
+                        </Typography>
+                    </Box>
+                    <Box sx={{
+                        display: "flex",
+                        alignItems: "center"
+                    }}
+                    marginTop={3}
+                    >
+                        <Rating name="read-only" value={4.5} precision={0.5} size="small" readOnly />
+                        <Typography variant="body2" component="p" marginLeft={0.5}>
+                            4.5
+                        </Typography>
+                        <Typography variant="body3" component="p" marginLeft={1.5}>
+                            (655 reviews)
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <Typography variant="h6" component="h3"
+                    </Box>
                 </Box>
-                <Box sx={{
-                    display: "flex",
-                    alignItems: "center"
-                }}>
-                  
-                </Box>
+
             </Paper>
 
         </Grid>
